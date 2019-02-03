b31dcd2987dd77c36f09c0eb18f68722176d178b
zinebadi <45413450+zinebadi@users.noreply.github.com>
2019-02-03 11:28:43 +0800

    Update vigenere1.c

--- vigenere1.c
+++ vigenere1.c
@@ -4,142 +4,62 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-// A program that encrypts messages using Caesar’s cipher. Your program must//
-
 int main(int argc, string argv[])
 {
-  //chechk if argc is equal to 2 //
-  if (argc != 2)
-    {
-    printf("error more or less than 2\n");
-    return 1;
-    }
-    else
-    {
-      string key = argv[1];
-
-      for ( int j = 0; j < strlen(key); j++)
-      {
-        if (isalpha(key[j]))
-        {
-        }
-        else
-        {
-          printf("Error your key is not alphabetical\n");
-          return 2;
-        }
-      }
-
-      //get the plaintext//
-      printf("what s your message?\n");
-      string pt = get_string();
-    }
-}
-        
-      for ( int j = 0; j < strlen(key); j++)
-      {
-        for (int i = 0; i < strlen(pt); i++)
-         {
-          if (isalpha (pt[i]))
-            {
-                if (isupper (pt[i]))
-                {
-                  if (isupper (key[j]))
-                  {
-                     printf("%c", ((((pt[i] + key[j]) - 130) % 26) + 65));
-                  }
-                  else
-                  {
-                    printf("%c", ((((pt[i] + key[j]) - 162) % 26) + 65));
-                  }
-                
-                {
-                  printf("%c", ((((pt[i] + key[j]) - 130) % 26) + ));
-                }
-
-              else if (islower(pt[i]) && islower(key[j]))
-                {
-                  printf("%c", ((((pt[i] + key) - 194 )% 26) + 194));
-                }
-              else 
-              {
-                printf("%c", ((((pt[i] + key) - 162 )% 26) + 162));
-              }
-         
-                
-             }
-            else
-            {
-               printf("%c", pt[i]);
-            }
-          }
-        printf("\n");
-        return 0;
-        }
-    }
-}
-
-
-while(
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-while ( n = strlen(pt) % strlen(key) )
-  
- 
-  
-for (int i = 0; i < strlen(pt); i++)
+  if (argc == 2)
   {
-    int n = i % strlen(key);
+    string key = argv[1];
     
-    
-      if (isalpha (pt[i]))
-            {
-                if (isupper (pt[i]))
+    for (int i=0; i <strlen(key); i++)
+    {
+      if (!isalpha(key[i])
+        {        
+          printf("ur key is not valid");
+          error 2;
+        }
+     }
+        printf("your plaintext: ");
+        string pt = get_string();
+        int counter = 0;  
+          
+        //read each charachter of the plaintext //
+          for (int j =0; j < strlen(pt); j++)
+          {
+            if (!isalpha(pt[j])
                 {
-                  if (isupper (key[j]))
-                  {
-                     printf("%c", ((((pt[i] + key[j]) - 130) % 26) + 65));
-                  }
-                  else
-                  {
-                    printf("%c", ((((pt[i] + key[j]) - 162) % 26) + 65));
-                  }
+                  counter++;
+                  printf("%c", pt[j]);
                 }
                 else
                 {
-                  if (isupper (key[j]))
+                  i = ((j-counter)%strlen(key));
+                  if (isupper (pt[j]))
                   {
-                     printf("%c", ((((pt[i] + key[j]) - 162) % 26) + 97));
+                    if (isupper (key[i]))
+                    {
+                      printf("%c", ((((pt[j] + key[i]) - 130) % 26) + 65));
+                    }
+                    else
+                    {
+                      printf("%c", ((((pt[j] + key[i]) - 162) % 26) + 65));
+                    }
                   }
                   else
                   {
-                    printf("%c", ((((pt[i] + key[j]) - 194) % 26) + 97));
+                    if (isupper (key[i]))
+                    {
+                      printf("%c", ((((pt[j] + key[i]) - 162) % 26) + 97));
+                    }
+                    else
+                    {
+                      printf("%c", ((((pt[j] + key[i]) - 194) % 26) + 97));
+                    }
                   }
-                }
             }
-            else
-            {
-              printf("%c", pt[i]);
-            }
+        }
   }
-      
-
-
-
-
-
-
-
-
-
-
-
+  else 
+  {
+    printf("Try Again with 2!!");
+    error 1;    
+  }
