70e5d73cad37367899267d30c1352f9560276294
zinebadi <45413450+zinebadi@users.noreply.github.com>
2019-02-05 04:38:44 +0800

    Update vigenere1.c

--- vigenere1.c
+++ vigenere1.c
@@ -1,65 +1,85 @@
 #include <stdio.h>
-#include <cs50.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <stdlib.h>
+#include <cs50.h>
 
 int main(int argc, string argv[])
 {
-  if (argc == 2)
-  {
-    string key = argv[1];
-    
-    for (int i = 0; i < strlen(s); i++)
+   // j is the Jth number in the key character & k is the result of key character - the ASCII Number 
+    int j;
+    int k;
+    int l = 0;
+
+    //Make sure there is only one command line argument
+    if (argc != 2)
     {
-        if (isalpha(s[i]) == false)
+        printf("Try Again\n");
+        return 1;
+    }
+
+
+    string key = (argv[1]);
+    //Make sure command line argument is alpha
+    for (int i = 0; i < strlen(key); i++)
+    {
+        if (isalpha(key[i]) == false)
         {
             printf("Try Again\n");
-            return 1;
+            return 2;
         }
     }
-        printf("your plaintext: ");
-        string pt = get_string();
-        int counter = 0;  
-          
-        //read each charachter of the plaintext //
-          for (int j =0; j < strlen(pt); j++)
+
+    //Get user input string
+    string plainText = get_string("plaintext: ");
+
+    //Place to print cipher text
+    printf("ciphertext: ");
+
+    for (int i = 0; i < strlen(plainText); i++)
+    {
+        //Encrypt lower case of plaintext
+      
+        if islower(plainText[i])
+        {
+          if isupper(key[j])  
           {
-            if (!isalpha(pt[j])
-                {
-                  counter++;
-                  printf("%c", pt[j]);
-                }
-                else
-                {
-                  i = ((j-counter)%strlen(key));
-                  if (isupper (pt[j]))
-                  {
-                    if (isupper (key[i]))
-                    {
-                      printf("%c", ((((pt[j] + key[i]) - 130) % 26) + 65));
-                    }
-                    else
-                    {
-                      printf("%c", ((((pt[j] + key[i]) - 162) % 26) + 65));
-                    }
-                  }
-                  else
-                  {
-                    if (isupper (key[i]))
-                    {
-                      printf("%c", ((((pt[j] + key[i]) - 162) % 26) + 97));
-                    }
-                    else
-                    {
-                      printf("%c", ((((pt[j] + key[i]) - 194) % 26) + 97));
-                    }
-                  }
-            }
+            j = (l % strlen(key));
+            k = (key[j]) - 65);
+            printf("%c", (((plainText[i] + k) - 97) % 26) + 97);
+            l += 1;
+          }
+          else 
+          {
+            j = (l % strlen(key));
+            k = (key[j]) - 97);
+            printf("%c", (((plainText[i] + k) - 97) % 26) + 97);
+            l += 1;
+          }
+       
+        //encrypt upper case of plaintext
+        else if isupper(plainText[i])
+        {
+            if isupper(key[j])  
+          {
+            j = (l % strlen(key));
+            k = (key[j]) - 65);
+            printf("%c", (((plainText[i] + k) - 65) % 26) + 65);
+            l += 1;
+          }
+          else 
+          {
+            j = (l % strlen(key));
+            k = (key[j]) - 97);
+            printf("%c", (((plainText[i] + k) - 65) % 26) + 65);
+            l += 1;
+          }
+        //print anything else
+        else
+        {
+            printf("%c", plainText[i]);
         }
-  }
-  else 
-  {
-    printf("Try Again with 2!!");
-    error 1;    
-  } ghaer
+    }
+    printf("\n");
+    return 0;
+}
